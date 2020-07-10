751cc251642176294df0287d5e026c5ae6fb6da7
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-07-10 15:16:42 +0530

    project solution for c109

---
+++ studentPerformance.py
@@ -0,0 +1,37 @@
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+import plotly.graph_objects as go
+import statistics
+import random
+#reading scores data
+df = pd.read_csv("StudentsPerformance.csv")
+data = df["reading score"].tolist()
+#Calculating the mean and the standard deviation
+mean = sum(data) / len(data)
+std_deviation = statistics.stdev(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+#Finding 1 standard deviation stard and end values, and 2 standard deviations stard and end values
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+#Plotting the chart, and lines for mean, 1 standard deviation and 2 standard deviations
+fig = ff.create_distplot([data], ["reading scores"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+fig.show()
+#Printing the findings
+list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
+list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
+list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
+print("Mean of this data is {}".format(mean))
+print("Median of this data is {}".format(median))
+print("Mode of this data is {}".format(mode))
+print("Standard deviation of this data is {}".format(std_deviation))
+print("{}% of data lies within 1 standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 2 standard deviations".format(len(list_of_data_within_2_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 3 standard deviations".format(len(list_of_data_within_3_std_deviation)*100.0/len(data)))
